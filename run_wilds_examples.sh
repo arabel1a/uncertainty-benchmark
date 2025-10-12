60061025ecb7fd6bff3efc2c846160a182e14cd8
arabel1a <your@mom.ru>
2025-10-12 11:37:17 +0300

    updated wilds_git

--- run_wilds_examples.sh
+++ run_wilds_examples.sh
@@ -1,8 +1,8 @@
 source venv/bin/activate
-export PATH=$(pwd)/wilds/examples:$PATH
+export PATH=$(pwd)/wilds_git/examples:$PATH
 
 # python3 wilds/examples/run_expt.py --algorithm ERM --dataset iwildcam --root_dir data
 
-# python3 wilds/examples/run_expt.py --algorithm ERM --dataset civilcomments --root_dir data
+# python3 wilds_git/examples/run_expt.py --algorithm ERM --dataset civilcomments --root_dir data
 
 python3 evaluate_uncertainty.py --algorithm ERM --dataset civilcomments --root_dir data
